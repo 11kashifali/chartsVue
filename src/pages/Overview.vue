6931ff2abaead2151eeccbe83d4cab3cfd3c2ae9
EvilCorps <11kashifali@gmail.com>
2018-06-30 00:12:28 +0500

    Second Commit

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="main">
-    <chartjs-line :height="110" v-if="mylabels" :labels="mylabels" :datasets="mydatasets"></chartjs-line>
+    <div class="container">
+      <date-picker class="datepicker" :lang="lang" v-model="time" format="YYYY-MM-DD" ></date-picker>
+      <div v-on:click="showmenu=!showmenu" class="datewise">Monthly
+        <ul v-show="showmenu" class="innermenu">
+          <li>Daily</li>
+          <li>Weekly</li>
+          <li>Monthly</li>
+        </ul>
+      </div>
+    </div>
+    <chartjs-line :option="myoption" :height="110" v-if="mylabels" :labels="mylabels" :datasets="mydatasets" :bind="true"></chartjs-line>
     <div>
       <h5 style="margin:20px 0;">TopPageViews</h5>
     </div>
@@ -21,18 +31,44 @@
   </div>
 </template>
 <script>
-  import 'chart.js';
+  import 'chart.js'
+  import DatePicker from 'vue2-datepicker'
   export default {
+    components:{
+      DatePicker
+    },
     mounted() {
       this.mylabels = null;
       this.axios.get('dataforcharts.json').then((response) => {
-        console.log(response.data)
         var self = this;
         setTimeout(function(){
                 self.mylabels = response.data.mylabels;
                 self.mydatasets = response.data.mydatasets;
         }, 400);
-        
+        var vm = this;
+        this.myoption = {
+            onClick:function(evt){
+                if(this.getElementAtEvent(evt).length == 0)
+                {
+                  return;
+                }
+                if(this.getElementAtEvent(evt)[0]._datasetIndex == 0){
+                  vm.$router.push({ path: '' })    
+                }
+                else if(this.getElementAtEvent(evt)[0]._datasetIndex == 1){
+                  vm.$router.push({ path: 'explore' })
+                }
+                else if(this.getElementAtEvent(evt)[0]._datasetIndex == 2){
+                  vm.$router.push({ path: 'realuser' })
+                }
+                else if(this.getElementAtEvent(evt)[0]._datasetIndex == 3){
+                  vm.$router.push({ path: 'returninguser' })
+                }
+                else if(this.getElementAtEvent(evt)[0]._datasetIndex == 4){
+                  vm.$router.push({ path: 'bots' })
+                }
+              }
+            };
       })
       this.axios.get('table.json').then((response) =>{
           this.tabledata = response.data.pageviews;
@@ -40,9 +76,21 @@
     },
     data() {
       return {
+        showmenu:false,
+        time: '',
+        lang: {
+        days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        pickers: ['next 7 days', 'next 30 days', 'previous 7 days', 'previous 30 days'],
+        placeholder: {
+          date: 'Select Date',
+          dateRange: 'Select Date Range'
+        }
+      },
         mylabels: null,
         mydatasets: null,
-        tabledata:null
+        tabledata:null,
+        myoption:null
       };
     },
   }
@@ -68,5 +116,40 @@
 }
 table{
     margin-bottom: 30px;
+}
+.container{
+  display: inline-block;
+  width: 100%;
+  text-align: right;
+  margin-bottom: 20px;
+}
+.container .datepicker{
+  float: right;
+}
+.datewise{
+  padding: 5px;
+  color:rgba(29, 165, 255);
+  float: right;
+  margin-right: 10px;
+  background: rgba(29, 165, 255, 0.226);
+  border: 1px solid rgba(29, 165, 255);
+  border-radius: 4px;
+  cursor: pointer;
+  position:relative;
+}
+.datewise ul{
+  position: absolute;
+  background-color: white;
+  padding: 5px;
+  left: 0;
+  top: 105%;
+  width: 120%;
+  border: 1px solid rgb(177, 177, 177);
+  border-radius: 5px;
+  color:black;
+}
+.datewise ul li{
+  text-align: left;
+  list-style: none;
 }
 </style>
